3d24ad43f8fc58f40a8d3f847232b4301df52de7
caseyWebb <notcaseywebb@gmail.com>
2016-06-28 15:57:12 -0400

    export from index

--- packages/ko-contrib-utils/lib/index.js
+++ packages/ko-contrib-utils/lib/index.js
@@ -1,17 +1,21 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['knockout', './defaults', './fromJS', './merge', './increment', './subscribeOnce', './toString'], factory);
+    define(['exports', 'knockout', './defaults', './fromJS', './merge', './increment', './subscribeOnce', './toString'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(require('knockout'), require('./defaults'), require('./fromJS'), require('./merge'), require('./increment'), require('./subscribeOnce'), require('./toString'));
+    factory(exports, require('knockout'), require('./defaults'), require('./fromJS'), require('./merge'), require('./increment'), require('./subscribeOnce'), require('./toString'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(global.knockout, global.defaults, global.fromJS, global.merge, global.increment, global.subscribeOnce, global.functionToStringNativeCode);
+    factory(mod.exports, global.knockout, global.defaults, global.fromJS, global.merge, global.increment, global.subscribeOnce, global.functionToStringNativeCode);
     global.index = mod.exports;
   }
-})(this, function (_knockout, _defaults, _fromJS, _merge) {
+})(this, function (exports, _knockout, _defaults, _fromJS, _merge) {
   'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
 
   var _knockout2 = _interopRequireDefault(_knockout);
 
@@ -30,4 +34,10 @@
   _knockout2.default.utils.defaults = _defaults2.default;
   _knockout2.default.utils.fromJS = _fromJS2.default;
   _knockout2.default.utils.merge = _merge2.default;
+
+  exports.default = {
+    defaults: _defaults2.default,
+    fromJS: _fromJS2.default,
+    merge: _merge2.default
+  };
 });
