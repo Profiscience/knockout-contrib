33546d470448b697275e985aee80bcee195f34f9
Casey Webb <notcaseywebb@gmail.com>
2016-12-15 13:46:13 -0600

    Add class support

--- packages/ko-contrib-utils/test/fromJS.js
+++ packages/ko-contrib-utils/test/fromJS.js
@@ -9,8 +9,23 @@ test('should create a deep observable tree', (t) => {
     date: new Date(),
     bool: true,
     arr: [],
+    regexp: /foo/,
     obj: { foo: 'foo' },
-    obs: ko.observable({ foo: 'bar' })
+    obs: ko.observable({ foo: 'bar' }),
+    func() {},
+    class: new class {
+      constructor() {
+        this.num = 1
+        this.str = 'str'
+        this.date = new Date()
+        this.bool = true
+        this.arr = []
+        this.obj = { foo: 'foo' }
+        this.obs = ko.observable({ foo: 'bar' })
+        this.regexp = /foo/
+      }
+      func() {}
+    }
   }
 
   const actual = fromJS(raw)
@@ -20,10 +35,24 @@ test('should create a deep observable tree', (t) => {
   t.true(ko.isObservable(actual.date))
   t.true(ko.isObservable(actual.bool))
   t.true(ko.isObservable(actual.arr))
+  t.true(ko.isObservable(actual.regexp))
   t.false(ko.isObservable(actual.obj))
   t.true(ko.isObservable(actual.obj.foo))
   t.true(ko.isObservable(actual.obs))
   t.false(ko.isObservable(actual.obs().foo))
+  t.false(ko.isObservable(actual.func))
+  t.false(ko.isObservable(actual.class))
+
+  t.true(ko.isObservable(actual.class.num))
+  t.true(ko.isObservable(actual.class.str))
+  t.true(ko.isObservable(actual.class.date))
+  t.true(ko.isObservable(actual.class.bool))
+  t.true(ko.isObservable(actual.class.arr))
+  t.true(ko.isObservable(actual.class.regexp))
+  t.false(ko.isObservable(actual.class.obj))
+  t.true(ko.isObservable(actual.class.obj.foo))
+  t.true(ko.isObservable(actual.class.obs))
+  t.false(ko.isObservable(actual.class.obs().foo))
 
   t.deepEqual(ko.toJS(actual), ko.toJS(raw))
 })
