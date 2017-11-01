5cf733dd2083bbab3a9b80501e04873419663687
Casey Webb <notcaseywebb@gmail.com>
2017-11-01 18:08:00 -0500

    Convert obeservable.fn packages to use jest

---
+++ packages/observable.fn.subscribeOnce/test.ts
@@ -0,0 +1,26 @@
+import 'jest'
+import * as ko from 'knockout'
+import './'
+
+test('subscribes once, then disposes', () => {
+  expect.assertions(4)
+  
+  const observable = ko.observable(null)
+  const observableArray = ko.observableArray([null])
+  const computed = ko.computed(() => observable())
+  const pureComputed = ko.pureComputed(() => observable())
+  
+  const hit = (v: any) => expect(v).toContain('foo')
+
+  observable.subscribeOnce(hit)
+  computed.subscribeOnce(hit)
+  pureComputed.subscribeOnce(hit)
+  observableArray.subscribeOnce(hit)
+
+  observable('foo')
+  observableArray(['foo'])
+
+  // expect.assertions(4)  would cause these to fail
+  observable('bar')
+  observableArray.push('bar')
+})
