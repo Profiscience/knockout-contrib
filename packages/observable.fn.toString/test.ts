5ae2f17a3c9ff85e51292626af48ddd14e5a3769
Casey Webb <notcaseywebb@gmail.com>
2017-11-01 18:08:00 -0500

    Convert obeservable.fn packages to use jest

---
+++ packages/observable.fn.toString/test.ts
@@ -0,0 +1,15 @@
+import 'jest'
+import * as ko from 'knockout'
+import './'
+
+test('returns the type and value', () => {
+  const observable = ko.observable(null)
+  const observableArray = ko.observableArray([null])
+  const computed = ko.computed(() => null)
+  const pureComputed = ko.pureComputed(() => null)
+
+  expect(observable.toString()).toBe('Observable(null)')
+  expect(observableArray.toString()).toBe('ObservableArray([null])')
+  expect(computed.toString()).toBe('Computed(null)')
+  expect(pureComputed.toString()).toBe('Computed(null)')
+})
