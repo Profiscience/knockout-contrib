a03ca6ffc6439d77fa7a1bc2c6e68efff0be36b4
Casey Webb <notcaseywebb@gmail.com>
2017-12-20 10:52:24 -0600

    Remove redundant ts declaration

--- packages/framework/src/route/component.d.ts
+++
@@ -1,8 +0,0 @@
-import { ViewModelConstructorBuilder } from '../model/builders/ViewModelConstructorBuilder'
-
-declare module '@profiscience/knockout-contrib-router' {
-  // tslint:disable-next-line no-shadowed-variable
-  interface IContext {
-    viewModel?: ViewModelConstructorBuilder
-  }
-}
