f647add6c1d1e68f3da2a00a894d335a00aaf3d9
Casey Webb <notcaseywebb@gmail.com>
2017-12-26 13:32:27 -0600

    Register observable.fn generator

--- plopfile.js
+++ plopfile.js
@@ -25,4 +25,22 @@ module.exports = (plop) => {
       }
     ]
   })
+  plop.setGenerator('observable.fn', {
+    description: 'Create a new observable function',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'function name'
+      }
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'packages/observable.fn.{{ kebabCase name }}',
+        templateFiles: 'templates/observable.fn/*',
+        base: 'templates/observable.fn'
+      }
+    ]
+  })
 }
