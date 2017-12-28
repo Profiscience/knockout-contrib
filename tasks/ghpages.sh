97d34090ea8820d0a6222dcdf9e7dede0cded5cb
Casey Webb <notcaseywebb@gmail.com>
2017-12-28 11:31:39 -0600

    :construction: WIP

--- tasks/ghpages.sh
+++ tasks/ghpages.sh
@@ -9,14 +9,13 @@ echo $BRANCH
     yarn build:docs;
     yarn build:examples;
     cp ./_config.yml dist;
-    rm -rf node_modules
     find . -name '*.md' -exec cp --parents \{\} dist \;
     ( cd dist
     git init
     git config user.name "Travis CI"
     git config user.email "travis@profiscience.com"
     git add .
-    git commit -m "chore(gh-pages)"
-    git push --force --quiet "https://${GH_TOKEN}@github.com/profiscience/knockout-contrib.git" master:gh-pages > /dev/null 2>&1
+    git commit -m "Publish gh-pages"
+    git push --force "https://${GH_TOKEN}@github.com/profiscience/knockout-contrib.git" master:gh-pages > /dev/null 2>&1
     )
 # fi
