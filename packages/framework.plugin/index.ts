9c8343077e27d571917e9fcdb977a794e25bf9ac
Casey Webb <notcaseywebb@gmail.com>
2018-01-08 13:11:46 -0600

    Decouple framework plugin from component plugin, pull into own package

---
+++ packages/framework.plugin/index.ts
@@ -0,0 +1,24 @@
+import * as ko from 'knockout'
+import { Context, IContext, IRouteConfig } from '@profiscience/knockout-contrib-router'
+import { INITIALIZED } from '@profiscience/knockout-contrib-framework-model-builders-data'
+
+// for ctx.component type definition
+import '@profiscience/knockout-contrib-router-plugins-component'
+
+export function frameworkPlugin() {
+  return (ctx: Context & IContext) => {
+    if (!ctx.component) return
+
+    ctx.queue((async () => {
+      const { viewModel } = await ctx.component
+      if (viewModel) {
+        await Promise.all(
+          Object
+          .keys(viewModel)
+          .filter((prop: any) => (viewModel as any)[prop][INITIALIZED])
+          .map(async (dataModelProp: any) => await (viewModel as any)[dataModelProp][INITIALIZED])
+        )
+      }
+    })())
+  }
+}
--- packages/framework/src/route/index.ts
+++
@@ -1,8 +0,0 @@
-import * as ko from 'knockout'
-import { Context, IContext, Route } from '@profiscience/knockout-contrib-router'
-import { componentPlugin } from './component'
-import { componentsPlugin } from './components'
-
-Route
-  .usePlugin(componentPlugin)
-  .usePlugin(componentsPlugin)
