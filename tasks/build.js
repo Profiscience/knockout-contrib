950af3fce9b0d104e375f44eb800ecc72c200c8f
Casey Webb <notcaseywebb@gmail.com>
2018-02-01 17:52:18 -0600

    Don't bundle

--- tasks/build.js
+++ tasks/build.js
@@ -6,21 +6,16 @@ const { without } = require('lodash')
 const PACKAGE_PATH = process.cwd()
 const PACKAGE_NAME = path.basename(PACKAGE_PATH)
 const pkg = require(path.join(PACKAGE_PATH, 'package.json'))
-const tsconfig = require(path.join(PACKAGE_PATH, 'tsconfig.json'))
 const dist = path.join(PACKAGE_PATH, 'dist')
 
 exports.build = function* (task) {
   const tasks = ['transpile']
-  const isNodePkg = tsconfig.compilerOptions.module === 'commonjs' // e.g. jest-matchers
   const isComponent = /components\./.test(PACKAGE_NAME)
 
   yield task.clear([dist, ...without(pkg.files, 'webpack.js')])
 
   if (isComponent) {
     tasks.unshift('styles')
-  }
-  if (!isNodePkg) {
-    tasks.push('bundle')
   }
 
   yield task.serial(tasks)
