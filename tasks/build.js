580c9e61d2137b76f63f37dbe73021f0e657585c
Casey Webb <notcaseywebb@gmail.com>
2018-02-04 09:54:04 -0600

    Some logging for travis

--- tasks/build.js
+++ tasks/build.js
@@ -8,6 +8,10 @@ const PACKAGE_NAME = path.basename(PACKAGE_PATH)
 const pkg = require(path.join(PACKAGE_PATH, 'package.json'))
 const dist = path.join(PACKAGE_PATH, 'dist')
 
+if (process.env.TRAVIS) {
+  console.log(`\n🚀  Building in ${PACKAGE_NAME}`)
+}
+
 exports.build = function* (task) {
   const tasks = ['transpile']
   const isComponent = /components\./.test(PACKAGE_NAME)
