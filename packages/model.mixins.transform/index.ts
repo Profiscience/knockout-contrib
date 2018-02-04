fd490ae52545ab9eb3d8af4358c60a6cc8a3f941
Casey Webb <notcaseywebb@gmail.com>
2018-02-04 04:02:42 -0600

    [model.mixins.transform] Call transform with this

--- packages/model.mixins.transform/index.ts
+++ packages/model.mixins.transform/index.ts
@@ -1,13 +1,13 @@
 import * as ko from 'knockout'
 import { DataModelConstructorBuilder } from '@profiscience/knockout-contrib-model-builders-data'
 
-export function TransformMixin(transform: (fetchData: any) => any) {
+export function TransformMixin(transform: (this: any, fetchData: any) => any) {
   return <
-    P extends { page: number }, T extends { new(...args: any[]): DataModelConstructorBuilder<P> }
+    P extends {}, T extends { new(...args: any[]): DataModelConstructorBuilder<P> }
   >(ctor: T) => class extends ctor {
     protected async fetch() {
       const res = await super.fetch()
-      return transform(res)
+      return transform.call(this, res)
     }
   }
 }
