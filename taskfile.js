b7ccae89bdf09fb47e33bc0c97d6af2144a353b2
Casey Webb <notcaseywebb@gmail.com>
2018-02-04 07:54:44 -0600

    Fix build process

--- taskfile.js
+++ taskfile.js
@@ -2,8 +2,12 @@
 
 const fs = require('fs')
 const path = require('path')
+const { endsWith } = require('lodash')
 const tasks = fs.readdirSync(path.resolve(__dirname, 'tasks'))
 
 for (const t of tasks) {
+  if (endsWith(t, '.ts')) {
+    continue
+  }
   Object.assign(exports, require(`./tasks/${t}`))
 }
