c35a70a1aad1a8382734bc0e737edd9afc38276a
Casey Webb <notcaseywebb@gmail.com>
2018-07-02 15:43:57 -0500

    fix(meta): Export end-user model builders and mixins

--- packages/_/index.ts
+++ packages/_/index.ts
@@ -7,6 +7,11 @@ export * from '@profiscience/knockout-contrib-bindings-toggle'
 
 export * from '@profiscience/knockout-contrib-filters-date-format'
 
+export * from '@profiscience/knockout-contrib-model-builders-data'
+export * from '@profiscience/knockout-contrib-model-builders-view'
+export * from '@profiscience/knockout-contrib-model-mixins-pager'
+export * from '@profiscience/knockout-contrib-model-mixins-transform'
+
 export * from '@profiscience/knockout-contrib-router'
 export * from '@profiscience/knockout-contrib-router-middleware-scroll-position'
 export * from '@profiscience/knockout-contrib-router-plugins-children'
