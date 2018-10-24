d0fa8fb106ed7298a1494680ccb1fee4d95bc35f
Casey Webb <notcaseywebb@gmail.com>
2018-10-24 17:32:35 -0500

    fix: Export createLoadingMiddleware from metapackage

--- packages/_/index.ts
+++ packages/_/index.ts
@@ -20,6 +20,7 @@ export * from '@profiscience/knockout-contrib-query'
 
 export * from '@profiscience/knockout-contrib-router'
 export * from '@profiscience/knockout-contrib-router-middleware-flash-message'
+export * from '@profiscience/knockout-contrib-router-middleware-loading'
 export * from '@profiscience/knockout-contrib-router-middleware-progress-bar'
 export * from '@profiscience/knockout-contrib-router-middleware-scroll-position'
 export * from '@profiscience/knockout-contrib-router-plugins-children'
