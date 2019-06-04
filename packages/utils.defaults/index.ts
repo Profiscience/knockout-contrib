bfd9300d7fb7874e35775ca66b282d7337f095aa
Casey Webb <notcaseywebb@gmail.com>
2019-06-03 20:35:30 -0500

    feat(utils.defaults): Improve type soundness

--- packages/utils.defaults/index.ts
+++ packages/utils.defaults/index.ts
@@ -2,16 +2,27 @@ import * as ko from 'knockout'
 import { fromJS } from '@profiscience/knockout-contrib-utils-from-js'
 
 export function defaults<
-  T extends Record<string, any | ko.Observable | ko.Computed | void>
->(dest: T, defaultValues: Record<keyof T, any>, mapArraysDeep = false): T {
+  TSrc extends Record<string, any>,
+  TDest extends {
+    [P in keyof TSrc]?: TSrc[P] extends []
+      ? ko.ObservableArray<TSrc[P][0]>
+      : ko.Observable<TSrc[P]>
+  }
+>(dest: TDest, defaultValues: TSrc, mapArraysDeep = false): TDest {
   for (const prop in defaultValues) {
-    if (isUndefined(dest[prop])) {
-      dest[prop] = fromJS(
-        defaultValues[prop],
-        (defaultValues[prop] as any) instanceof Array && mapArraysDeep
-      )
-    } else if (ko.isObservable(dest[prop]) && isUndefined(dest[prop]())) {
-      dest[prop](defaultValues[prop])
+    if (defaultValues.hasOwnProperty(prop)) {
+      const maybeDestObservable = dest[prop]
+      if (isUndefined(maybeDestObservable)) {
+        dest[prop] = fromJS(
+          defaultValues[prop],
+          (defaultValues[prop] as any) instanceof Array && mapArraysDeep
+        )
+      } else if (
+        ko.isObservable(maybeDestObservable) &&
+        isUndefined(maybeDestObservable())
+      ) {
+        maybeDestObservable(defaultValues[prop])
+      }
     }
   }
   return dest
