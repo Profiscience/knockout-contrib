97697700a8587b6b98448b2eeda52eb34492eb18
Casey Webb <notcaseywebb@gmail.com>
2019-06-13 14:58:17 -0500

    fix(bindings.draggable): Export from metapackage

--- packages/_/index.ts
+++ packages/_/index.ts
@@ -2,6 +2,7 @@ export * from '@profiscience/knockout-contrib-bindings-click-alt'
 export * from '@profiscience/knockout-contrib-bindings-click-ctrl'
 export * from '@profiscience/knockout-contrib-bindings-click-meta'
 export * from '@profiscience/knockout-contrib-bindings-click-shift'
+export * from '@profiscience/knockout-contrib-bindings-draggable'
 export * from '@profiscience/knockout-contrib-bindings-jquery'
 export * from '@profiscience/knockout-contrib-bindings-toggle'
 
