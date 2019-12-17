7df2d84994f1956577290b4fe4142038021d4c22
Casey Webb <notcaseywebb@gmail.com>
2019-12-17 12:47:58 -0600

    feat(bindings.infinitescroll): initial commit

---
+++ packages/bindings.infiniteScroll/README.md
@@ -0,0 +1,44 @@
+# bindings.infiniteScroll
+
+[![Version][npm-version-shield]][npm]
+[![Dependency Status][david-dm-shield]][david-dm]
+[![Peer Dependency Status][david-dm-peer-shield]][david-dm-peer]
+[![Dev Dependency Status][david-dm-dev-shield]][david-dm-dev]
+[![Downloads][npm-stats-shield]][npm-stats]
+
+[david-dm]: https://david-dm.org/Profiscience/knockout-contrib?path=packages/bindings.infiniteScroll
+[david-dm-shield]: https://david-dm.org/Profiscience/knockout-contrib/status.svg?path=packages/bindings.infiniteScroll
+[david-dm-peer]: https://david-dm.org/Profiscience/knockout-contrib?path=packages/bindings.infiniteScroll&type=peer
+[david-dm-peer-shield]: https://david-dm.org/Profiscience/knockout-contrib/peer-status.svg?path=packages/bindings.infiniteScroll
+[david-dm-dev]: https://david-dm.org/Profiscience/knockout-contrib?path=packages/bindings.infiniteScroll&type=dev
+[david-dm-dev-shield]: https://david-dm.org/Profiscience/knockout-contrib/dev-status.svg?path=packages/bindings.infiniteScroll
+[npm]: https://www.npmjs.com/package/@profiscience/knockout-contrib-bindings-infinite-scroll
+[npm-version-shield]: https://img.shields.io/npm/v/@profiscience/knockout-contrib-bindings-infinite-scroll.svg
+[npm-stats]: http://npm-stat.com/charts.html?package=@profiscience/knockout-contrib-bindings-infinite-scroll&author=&from=&to=
+[npm-stats-shield]: https://img.shields.io/npm/dt/@profiscience/knockout-contrib-bindings-infinite-scroll.svg?maxAge=2592000
+
+> This package is intended for consumption via the [@profiscience/knockout-contrib][] metapackage
+
+Call a function when the end of an element is reached
+
+## Usage
+
+```html
+<div data-bind="foreach: todos, infiniteScroll: fetchMoreTodos">
+  ...
+</div>
+```
+
+**with a custom offset**
+
+By default the infinite scroll will trigger 1500px before reaching the bottom of the bound element. This can be customized, for example to preload slower APIs sooner you could define a larger offset.
+
+```html
+<div
+  data-bind="foreach: todos, infiniteScroll: { handler: fetchMoreTodos, offset: 2000 }"
+>
+  ...
+</div>
+```
+
+[@profiscience/knockout-contrib]: https://github.com/Profiscience/knockout-contrib
---
+++ packages/bindings.infiniteScroll/index.ts
@@ -0,0 +1,79 @@
+import * as ko from 'knockout'
+import throttle from 'lodash/throttle'
+
+type InfiniteScrollBindingCallback = () => void | Promise<void>
+
+type InfiniteScrollBindingOptions =
+  | InfiniteScrollBindingCallback
+  | {
+      offset: number
+      handler: InfiniteScrollBindingCallback
+    }
+
+export const infiniteScrollBindingHandler: ko.BindingHandler<InfiniteScrollBindingOptions> = {
+  init(el, valueAccessor, allBindings, viewModel, bindingContext) {
+    const opts = valueAccessor()
+    const { handler, offset } =
+      typeof opts === 'function' ? { offset: 1500, handler: opts } : opts
+
+    const $el = $(el)
+    const $window = $(window)
+
+    let prevScrolledDist: number = 0
+
+    const handleScroll = throttle((e) => {
+      const windowScrollTop = $window.scrollTop()
+      const windowHeight = $window.height()
+
+      if (
+        typeof windowScrollTop === 'undefined' ||
+        typeof windowHeight === 'undefined'
+      ) {
+        throw new Error(
+          '[knockout-contrib/bindings.infiniteScroll]: Error calculating window scrolled distance'
+        )
+      }
+
+      const scrolledDist = windowScrollTop + windowHeight
+
+      if (scrolledDist < prevScrolledDist) {
+        prevScrolledDist = scrolledDist
+        return
+      }
+      prevScrolledDist = scrolledDist
+
+      const elOffset = $el.offset()
+      const elHeight = $el.outerHeight()
+
+      if (typeof elOffset === 'undefined' || typeof elHeight === 'undefined') {
+        throw new Error(
+          '[knockout-contrib/bindings.infiniteScroll]: Error calculating element trigger point'
+        )
+      }
+      const triggerPoint = elOffset.top - elHeight - offset
+
+      if (scrolledDist < triggerPoint) return
+
+      const promise: void | Promise<void> = handler.call(bindingContext.$data)
+
+      disarmTrigger()
+
+      if (promise && typeof promise.then === 'function') {
+        promise.then(armTrigger)
+      } else {
+        requestAnimationFrame(armTrigger)
+      }
+    }, 300)
+
+    function armTrigger() {
+      document.addEventListener('scroll', handleScroll)
+    }
+
+    function disarmTrigger() {
+      document.removeEventListener('scroll', handleScroll)
+    }
+
+    armTrigger()
+    ko.utils.domNodeDisposal.addDisposeCallback(el, disarmTrigger)
+  }
+}
