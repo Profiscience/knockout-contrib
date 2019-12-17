c372774aae2bf62fe298ac0d1178ea1c285a6cba
Casey Webb <notcaseywebb@gmail.com>
2019-12-17 12:51:43 -0600

    fix(metapackage): actually re-export bindings.infiniteScroll

--- packages/_/index.ts
+++ packages/_/index.ts
@@ -3,6 +3,7 @@ export * from '@profiscience/knockout-contrib-bindings-click-ctrl'
 export * from '@profiscience/knockout-contrib-bindings-click-meta'
 export * from '@profiscience/knockout-contrib-bindings-click-shift'
 export * from '@profiscience/knockout-contrib-bindings-draggable'
+export * from '@profiscience/knockout-contrib-bindings-infinite-scroll'
 export * from '@profiscience/knockout-contrib-bindings-jquery'
 export * from '@profiscience/knockout-contrib-bindings-toggle'
 
